a82a25eb4dceea907d6529ff11e4f0d7082fa0bc
Robert Smart <roca1388@gmail.com>
2022-02-04 12:52:01 -0300

    update

--- binario.js
+++ binario.js
@@ -1,12 +1,30 @@
 function convertir(n){
 let b = []
-while(n>1){
-    
+let c
+while(n!=0){ 
     b.push(n % 2)
     n= Math.floor(n/2)
-   
 }
 b.push(n)
-return parseInt(b.reverse().join(''))
+c=b.reverse()
+
+var max = 0;
+var cur = 0;
+for(var i=0; i<c.length; i++ ){
+    if(c[i]==1){
+        cur++;
+    }else if(c[i]==0){
+        if(cur>max){
+            max=cur
+            cur=0
+        }
+    }
 }
-console.log(convertir(3));
+if(cur>max){
+    max=cur;
+}
+console.log(max);
+console.log(c);
+return parseInt(c.join(''))
+}
+console.log(convertir(35));
