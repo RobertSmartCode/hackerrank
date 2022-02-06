247e537c9adb0fdcf5fd62e644d1e3d68ba109ac
Robert Smart <roca1388@gmail.com>
2022-02-06 14:37:09 -0300

    Scope

--- Scope.js
+++ Scope.js
@@ -1,17 +1,20 @@
 const inputLines= [ '3', '1 2 5' ]
+const length=+inputLines[0]
+const array= inputLines[1].split(' ')
 
-k=Number(inputLines.join().split(',',1).join())
-l=inputLines.reverse().join().split(',',1).join().split(' ')
-let Max=0
-for(var i=0; i<k; i++){
-    let J =Math.abs(l[0] - l[k]);
-    if(J>Max){
-        Max=J
+let max = 0;
+let M = 0;
+
+for(var i=0; i<length; i++){
+    for(var j=0; j<length; j++){
+        M= Math.abs(array[i]-array[j]);
+        if(M>max){
+            max=M
+        }
     }
-    let M=Math.abs(l[i] - l[i+1]);
-    console.log(M);
-    if(M>Max){
-        Max=M
-    }
+    
 }
-console.log(Max);
+
+console.log(max);
+
+
