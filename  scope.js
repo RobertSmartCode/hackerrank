358f9f47ee0c488d8d94307b7b218b8034fce72a
Robert Smart <roca1388@gmail.com>
2022-02-06 13:18:40 -0300

    scope

---
+++  scope.js
@@ -0,0 +1,4 @@
+const inputLines= [ '3', '1 2 5' ]
+
+k=inputLines.split(',')
+console.log(k); 
---
+++ Polymorphism.js
@@ -0,0 +1,28 @@
+
+
+class Animal {
+    constructor(name){
+        this.name=name;
+
+    }
+    makeSound(){
+        console.log('Generica Animal Sound');
+    }
+    
+}
+
+class Dog extends Animal{
+    constructor(name){
+        super()
+    }
+    makeSound(){
+        super.makeSound()
+        console.log('Guao Guao');
+    } 
+}
+
+const a1= new Animal('Dog')
+const a2= new Dog('Robert')
+
+a1.makeSound()
+a2.makeSound()
